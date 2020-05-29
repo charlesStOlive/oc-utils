d54ae6f8d406a87cfe18be984d1dc74a4cf4eebb
Charels <charles.stolive@gmail.com>
2020-05-29 14:09:07 +0200

    ajout de couleurs dans phpColors

--- classes/PhpColors.php
+++ classes/PhpColors.php
@@ -18,11 +18,23 @@ class PhpColors
         $color1 = new Color($mainColor);
         $this->complementaryColor = '#' . $color1->complementary();
         $complementaryColor = new Color($this->complementaryColor);
+        $red = $color1->mix('#ff0000', 60);
+        $blue = $color1->mix('#0000ff', 60);
+        $yellow = $color1->mix('#0000FF', 60);
+        $orange = $color1->mix('#FF6600', 60);
+        $green = $color1->mix('#00FF00', 60);
+        $purple = $color1->mix('#6600FF', 60);
 
         $this->colorArray = new Collection();
 
         $this->colorArray->push(['color' => $this->mainColor]);
         $this->colorArray->push(['color' => $this->complementaryColor]);
+        $this->colorArray->push(['color' => '#' . $red]);
+        $this->colorArray->push(['color' => '#' . $blue]);
+        $this->colorArray->push(['color' => '#' . $yellow]);
+        $this->colorArray->push(['color' => '#' . $orange]);
+        $this->colorArray->push(['color' => '#' . $green]);
+        $this->colorArray->push(['color' => '#' . $purple]);
         $this->colorArray->push(['color' => '#' . $color1->lighten(10)]);
         $this->colorArray->push(['color' => '#' . $color1->darken(10), 'darken' => true]);
         $this->colorArray->push(['color' => '#' . $complementaryColor->darken(10), 'darken' => true]);
