6847cd3854670d88e889dcaf6ceb7f3510405067
Charles <charles.stolive@gmail.com>
2021-03-15 15:28:05 +0100

    travail sur waka:trad

--- console/PluginTrad.php
+++ console/PluginTrad.php
@@ -5,6 +5,7 @@ use October\Rain\Support\Collection;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Twig;
+use File;
 
 class PluginTrad extends GeneratorCommand
 {
@@ -14,13 +15,19 @@ class PluginTrad extends GeneratorCommand
     public $wk_author;
     public $wk_model;
     public $remover;
+    //
+    private $originalFolder;
+    private $destinationFolder;
+    private $codeLang;
+    private $apiLimit;
+
 
     /**
      * The console command name.
      *
      * @var string
      */
-    protected $name = 'waka:plugintrad';
+    protected $name = 'waka:trad';
 
     /**
      * The console command description.
@@ -51,12 +58,9 @@ class PluginTrad extends GeneratorCommand
      */
     public function handle()
     {
-        $this->vars = $this->prepareVars(true);
-
-        $this->makeTrads();
-
+        $this->vars = $this->prepareVars();
+        $this->getModelLangPath();
         $this->info($this->type . 'created successfully.');
-
     }
 
     /**
@@ -64,64 +68,184 @@ class PluginTrad extends GeneratorCommand
      *
      * return @array
      */
-    protected function prepareVars($putTrans = false)
+    protected function prepareVars()
     {
-        $this->p_name = $name = $this->argument('name');
-
         $this->p_pluginCode = $pluginCode = $this->argument('plugin');
         $parts = explode('.', $pluginCode);
         $this->p_plugin = $plugin = array_pop($parts);
         $this->p_author = $author = array_pop($parts);
+        $this->codeLang = $this->argument('lang');
 
         $this->p_model = $model = $this->option('model');
     }
 
-    /**
-     * Make all stubs.
-     *
-     * @return void
-     */
-    public function makeTrads()
-    {
-        // $stubs = array_keys($this->stubs);
-
-        // foreach ($stubs as $stub) {
-        //     $this->makeStub($stub);
-        // }
+    protected function getModelLangPath() {
+        $pluginSelectedPath = plugins_path(strtolower($this->p_author) .'/'. strtolower ($this->p_plugin) .'/lang');
+        $this->originalFolder = $pluginSelectedPath .'/fr';
+        $this->destinationFolder = $pluginSelectedPath .'/'.$this->codeLang;
+        //TEST SI DOSSIER DESTTINATION EXISTE
+        File::isDirectory($this->destinationFolder) or File::makeDirectory($this->destinationFolder, 0775);
+        //
+        $this->apiLimit = 0;
+        $files = File::allfiles($this->originalFolder);
+        foreach($files as $file) {
+            $this->launchTradFile($file);
+        }
     }
 
     /**
-     * Make a single stub.
-     *
-     * @param string $stubName The source filename for the stub.
+     * 
      */
-    public function makeStub($stubName)
-    {
-        // if (!isset($this->stubs[$stubName])) {
-        //     return;
-        // }
+    public function launchTradFile($file) {
+        $fileName = $file->getRelativePathname();
+        $fileLangName = $this->destinationFolder.'/'.$fileName;
+        //trace_log($fileLangName);
+        $content = null;
+        $tradContent = null;
+        $fileExiste = false;
+        if(File::exists($fileLangName)) {
+            $fileExiste = true;
+            $content = include $file;
+            $tradContent = include $fileLangName;
+            $content = $this->array_diff_key_recursive($content, $tradContent);
+            trace_log($content);
+        } else {
+            $content = new Collection(include $file);
+        }
+        if(!$content) {
+            /**/trace_log("pas de contenu");
+            return;
+        }
+        
+        $traduction = $this->recursiveLaunchTrad($content);
 
-        // $sourceFile = $this->getSourcePath() . '/' . $stubName;
-        // $destinationFile = $this->getDestinationPath() . '/' . $this->stubs[$stubName];
-        // $destinationContent = $this->files->get($sourceFile);
-
-        // /*
-        //  * Parse each variable in to the destination content and path
-        //  */
-        // $destinationContent = Twig::parse($destinationContent, $this->vars);
-        // $destinationFile = Twig::parse($destinationFile, $this->vars);
-
-        // $this->makeDirectory($destinationFile);
-
-        // /*
-        //  * Make sure this file does not already exist
-        //  */
-        // if ($this->files->exists($destinationFile) && !$this->option('force')) {
-        //     throw new Exception('Stop everything!!! This file already exists: ' . $destinationFile);
-        // }
-
-        // $this->files->put($destinationFile, $destinationContent);
+        if($fileExiste) {
+            $traduction  = array_merge_recursive($tradContent, $traduction);
+        }
+    
+        //FICHIER TRADUCTION PRET CREATION DU FICHIER DE LANGUE
+        $stubLang = $this->getSourcePath() . '/trad/lang.stub';
+        $langStubContent = File::get($stubLang);
+        $destinationContent = Twig::parse($langStubContent, ['data' => $traduction]);
+        //trace_log($destinationContent);
+        File::put($fileLangName,$destinationContent);
     }
+    /**
+     * storeMapKey
+     * enregistre les clefs du tableau dans les valeurs d'un tableau
+     * si il y a un sous tableau la ligne 0 a le nom de la clef
+     */
+
+    public function recursiveLaunchTrad($rows) {
+        
+        $newMap  = [];
+        foreach($rows as $key=> $row) {
+            if(is_array($row)) {
+                $row =  $this->recursiveLaunchTrad($row);
+                $newMap[$key] =  $row;
+            } else {
+                $this->apiLimit++;
+                if($this->apiLimit > 99) {
+                    sleep(1);
+                    $this->apiLimit = 0;
+                }
+                $translation = \GoogleTranslate::translate($row, $this->codeLang);
+                $rowTraducted = $translation['translated_text'];
+                $newMap[$key] =  $rowTraducted;
+            }
+        }
+        return $newMap;
+    }
+
+    function array_diff_key_recursive (array $arr1, array $arr2) {
+        $diff = array_diff_key($arr1, $arr2);
+        $intersect = array_intersect_key($arr1, $arr2);
+        foreach ($intersect as $k => $v) {
+            if (is_array($arr1[$k]) && is_array($arr2[$k])) {
+                $d = $this->array_diff_key_recursive($arr1[$k], $arr2[$k]);
+                if ($d) {
+                    $diff[$k] = $d;
+                }
+            }
+        }
+        return $diff;
+    }
+    /**
+     * NE PAS SUPPRIMER OU A SAUVEGARDER AILLEUL
+     * ANCIENNE IDEE UTILITE POSSIBLE POUR AUTRE CODES
+     * storeMapKey permet de suvegarder les clefs d'un tableau dans un nouveau tableau en temps que valeur
+     * createMapRow : permet d'enlever des clefs d'un tableau
+     * fetchKey : remt les clef dun' tableau si il a les mêmes lignes que storeMzapKey.
+     *  Si dessous ancinne partie du code et les3 fonctions.
+     */
+        //Création du mappage de sauvegarde ( on va enlever les clefs avant de lancer la traduction.
+        //$contentStoredKey = $this->storeMapKey($content);
+        //trace_log($contentStoredKey);
+        //$contentMappedRow = $this->createMapRow($content);
+        //trace_log($contentMappedRow);
+        //TRADUCTION
+        // $contentJson = json_encode($contentMappedRow, JSON_UNESCAPED_UNICODE );
+        // trace_log($contentJson);
+        //trace_log($translation);
+        // $contentReturn = json_decode($translation['translated_text'],true,512);
+        // //trace_log($contentReturn);
+        // // $contentReturn = json_decode(File::get($this->originalFolder.'/json_comp.json'));
+        // $fetched = $this->fetchKey($contentStoredKey, $contentReturn);
+        // //trace_log($fetched);
+        // if($fileExiste) {
+        //     $fetched  = array_merge_recursive($tradContent, $fetched);
+        // } 
+        // //PERMET DE TESTER 
+        // //$contentReturn = json_decode(File::get($this->originalFolder.'/json_en.json'));
+
+
+    // public function storeMapKey($rows, $key = null) {
+    //     $newMap  = [];
+    //     if($key) {
+    //         array_push($newMap, $key);
+    //     }
+    //     foreach($rows as $key=> $row) {
+    //         if(is_array($row)) {
+    //             $row =  $this->storeMapKey($row, $key);
+    //             array_push($newMap, $row);
+    //         } else {
+    //             array_push($newMap, $key);
+    //         }
+    //     }
+    //     return $newMap;
+    // }
+    // public function createMapRow($rows) {
+    //     $newMap  = [];
+    //     foreach($rows as $key=> $row) {
+            
+    //         if(is_array($row)) {
+    //             $row =  $this->createMapRow($row);
+    //             array_push($newMap, $row);
+    //         } else {
+    //             array_push($newMap, $row);
+    //         }
+    //     }
+    //     return $newMap;
+    // }
+
+
+    // public function fetchKey($keyArray, $rowArray, $key=null) {
+    //     $newMap  = [];
+    //     foreach($keyArray as $key=>$row) {
+    //         if(is_array($row)) {
+    //             $newKey = array_shift($row);
+    //             $newRow =  $this->fetchKey($row,$rowArray[$key]);
+    //             $newMap[$newKey] = $newRow;
+    //         } else {
+    //             $newKey = $keyArray[$key];
+    //             $newRow = $rowArray[$key];
+    //             $newMap[$newKey] = $newRow;
+    //         }
+    //     }
+    //     return $newMap;
+    // }
+
+    
 
     /**
      * Get the console command arguments.
@@ -131,9 +255,8 @@ class PluginTrad extends GeneratorCommand
     protected function getArguments()
     {
         return [
-            ['name', InputArgument::REQUIRED, 'The name of the workflow'],
             ['plugin', InputArgument::REQUIRED, 'The name of the plugin. Eg: RainLab.Blog'],
-            
+            ['lang', InputArgument::REQUIRED, 'lang destination'],
         ];
     }
 
@@ -146,7 +269,7 @@ class PluginTrad extends GeneratorCommand
     {
         return [
             ['force', null, InputOption::VALUE_NONE, 'Overwrite existing files with generated ones.'],
-            ['model', InputArgument::REQUIRED, 'The name of the model. Eg: Post'],
+            ['model', null, InputOption::VALUE_NONE, 'The name of the model. Eg: Post'],
         ];
     }
 }
---
+++ lang/temp-copy/lang.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'menu' => [
+        'data_sources' => 'Sources / données',
+        'settings_category' => 'Configuration ',
+    ],
+    'global' => [
+        'details' => 'Détails',
+        'slug' => 'Slug',
+        'updated_at' => 'MAJ',
+        'created_at' => 'Crée-le',
+    ],
+    'datasource' => [
+        'title' => 'Source de données',
+        'inde_class_list' => [
+            'label' => 'Class indépendante à lier',
+            'prompt' => 'Entrez les classes indépendantes',
+        ],
+    ],
+    'job_list' => [
+        'name' => "Nom de la tache",
+        'started_at' => "Commencé le",
+        "scopes" => [
+            "not_end" => "Ne pas afficher les taches terminées",
+        ],
+    ],
+    'page' => [
+        'access_denied' => [
+            'label' => 'Accès impossible',
+        ],
+    ],
+
+];
