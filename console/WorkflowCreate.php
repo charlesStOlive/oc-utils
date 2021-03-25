adc397ac88fa8e4416b03dbf4e0ef9fb70717ba8
Charles <charles.stolive@gmail.com>
2021-03-25 13:57:29 +0100

    suppretrace

--- console/WorkflowCreate.php
+++ console/WorkflowCreate.php
@@ -100,7 +100,7 @@ class WorkflowCreate extends GeneratorCommand
         }
 
         $filePath =  $startPath.'/'.$fileName.'.xlsx';
-        trace_log($filePath);
+        //trace_log($filePath);
 
         if ($this->option('option')) {
             $this->remover = [
