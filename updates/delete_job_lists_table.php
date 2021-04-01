3af5860cd47275ec391f6f1340964d6797634d55
Charles <charles.stolive@gmail.com>
2021-04-01 16:03:18 +0200

    correction bug october:up

--- updates/delete_job_lists_table.php
+++ updates/delete_job_lists_table.php
@@ -4,7 +4,7 @@ use October\Rain\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
 use Schema;
 
-class CreateJobListsTable extends Migration
+class DeleteJobListsTable extends Migration
 {
     public function up()
     {
