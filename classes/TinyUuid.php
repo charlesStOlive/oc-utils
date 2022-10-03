d3f74681ceb2559c928a2b763e2abdcc7b2932d0
Charles <charles@notilac.fr>
2022-10-03 08:34:14 +0200

    ajout classe Tinyuuid

---
+++ classes/TinyUuid.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Waka\Utils\Classes;
+
+class TinyUuid
+{
+    public static function generate(int $length = 5)
+    {
+        $str = "";
+        for ($x = 0; $x < $length; $x++) $str .= substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"), 0, 1);
+        return $str;
+    }
+
+    public static function uuid(string $prefix = '', bool $more_entropy = false)
+    {
+        return uniqid($prefix, $more_entropy);
+    }
+
+    public static function readable(array $strings)
+    {
+        return implode('_', $strings);
+    }
+}
