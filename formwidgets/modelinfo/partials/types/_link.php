56accc76fd5d09457972d58a0f6665aaf3f7400b
Charles <charles@notilac.fr>
2023-05-12 13:50:49 +0200

    maj utilse avec les tooltype et les btn-col

--- formwidgets/modelinfo/partials/types/_link.php
+++ formwidgets/modelinfo/partials/types/_link.php
@@ -1,5 +1,5 @@
 <p><?php if ($data['link']): ?>
- <?= $data['label'] ?> : <a style="margin-top: 5px" class="oc-<?=$data['incon']?>" href="<?=$data['link']?>" target="_blank"><?= $data['value'] ?></a>
+ <?= $data['label'] ?> : <a style="margin-top: 5px" class="oc-<?=$data['icon']?>" href="<?=$data['link']?>" target="_blank"><?= $data['value'] ?></a>
 <?php else : ?>
    <?=$data['label'] ?? 'Inconnu'?>
 <?php endif ?>
